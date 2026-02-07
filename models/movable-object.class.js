b33741712f89b73d03c1aa2721c0f3e1ffc09447
Thomas Toebbe <toebbe.thomas@outlook.de>
2026-02-07 14:52:00 +0100

    feat: walking chicken

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,30 +1,28 @@
 class MovableObject {
-    x = 120;
-    y = 280;
-    img;
-    height = 150;
-    width = 100;
-    imageCache = {};
+  x = 120;
+  y = 280;
+  img;
+  height = 150;
+  width = 100;
+  imageCache = {};
+  currentImageIndex = 0;
 
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
+  loadImage(path) {
+    this.img = new Image();
+    this.img.src = path;
+  }
 
-    loadImages(arr) {
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-    }
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = img;
+    });
+  }
 
-    moveRight(pixels) {
-        console.log('Moving right');
-        
-    }
+  moveRight(pixels) {
+    console.log("Moving right");
+  }
 
-    moveLeft() {
-
-    }
+  moveLeft() {}
 }

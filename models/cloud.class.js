a9239a2a61d08619ab6f7a9dc45b1a48802e6a05
Thomas Toebbe <toebbe.thomas@outlook.de>
2026-02-07 12:00:42 +0100

    feat: moving clouds

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -7,6 +7,11 @@ constructor() {
     super().loadImage('img/5_background/layers/4_clouds/1.png');
  
     this.x = Math.random() * 500; 
-
+    this.animate();
+    }
+    animate() {
+        setInterval(() => {
+            this.x -= 0.15;
+        }, 1000 / 60);
     }
 }

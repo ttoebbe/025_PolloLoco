b38b0ac2a672cd652b7ba5e61f52d4ab2d75b6eb
Thomas Toebbe <toebbe.thomas@outlook.de>
2026-02-07 21:55:38 +0100

    perf: cameraposition and level end right and left

--- models/level.class.js
+++ models/level.class.js
@@ -2,6 +2,7 @@ class Level {
     enemies; 
     clouds;
     backgroundObjects;
+    level_end_x = 720 * 3;
 
     constructor(enemies, clouds, backgroundObjects) {
         this.enemies = enemies;
--- models/world.class.js
+++ models/world.class.js
@@ -1,8 +1,6 @@
 class World {
   character = new Character();
-  enemies = level1.enemies ;
-  clouds = level1.clouds ;
-  backgroundObjects = level1.backgroundObjects ;
+  level = level1;
   canvas;
   ctx;
   keyboard;
@@ -25,10 +23,10 @@ class World {
 
     this.ctx.translate(this.camera_x, 0);
 
-    this.addObjectsToMap(this.backgroundObjects);
+    this.addObjectsToMap(this.level.backgroundObjects);
     this.addToMap(this.character);
-    this.addObjectsToMap(this.enemies);
-    this.addObjectsToMap(this.clouds);
+    this.addObjectsToMap(this.level.enemies);
+    this.addObjectsToMap(this.level.clouds);
 
     this.ctx.translate(-this.camera_x, 0);
 

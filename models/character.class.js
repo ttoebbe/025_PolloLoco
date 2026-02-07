77d7fc01cd8918fb65027e67ec638259f1b049ab
Thomas Toebbe <toebbe.thomas@outlook.de>
2026-02-07 14:45:23 +0100

    feat. walking pepe

--- models/character.class.js
+++ models/character.class.js
@@ -1,12 +1,30 @@
 class Character extends MovableObject {
+  height = 250;
+  y = 180;
+  IMAGES_WALKING = [
+    "img/2_character_pepe/2_walk/W-21.png",
+    "img/2_character_pepe/2_walk/W-22.png",
+    "img/2_character_pepe/2_walk/W-23.png",
+    "img/2_character_pepe/2_walk/W-24.png",
+    "img/2_character_pepe/2_walk/W-25.png",
+    "img/2_character_pepe/2_walk/W-26.png",
+  ];
+  currentImageIndex = 0;
 
-    height = 250;
-    y = 180;
-constructor() {
-    super().loadImage('img/2_character_pepe/2_walk/W-21.png');
-}
+  constructor() {
+    super().loadImage("img/2_character_pepe/2_walk/W-21.png");
+    this.loadImages(this.IMAGES_WALKING);
+    this.animate();
+  }
 
-jump() {
+  animate() {
+    setInterval(() => {
+      let i = this.currentImageIndex % this.IMAGES_WALKING.length; // 0, 1, 2, 3, 4, 5 (Modulo-Operator)
+      let path = this.IMAGES_WALKING[i];
+      this.img = this.imageCache[path];
+      this.currentImageIndex++;
+    }, 100);
+  }
 
-}
+  jump() {}
 }

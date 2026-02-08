3e9ef50a64320b415890ee5ca8f1dd00ce358c4d
Thomas Toebbe <toebbe.thomas@outlook.de>
2026-02-08 16:16:41 +0100

    fix: end level

--- models/level.class.js
+++ models/level.class.js
@@ -2,7 +2,7 @@ class Level {
     enemies; 
     clouds;
     backgroundObjects;
-    level_end_x = 720 * 3;
+    level_end_x = 720 * 3.5;
 
     constructor(enemies, clouds, backgroundObjects) {
         this.enemies = enemies;

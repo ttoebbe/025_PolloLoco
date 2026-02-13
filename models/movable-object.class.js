91462865e664704fb8041e5d482195e98b326c54
Thomas Többe <toebbe.thomas@outlook.de>
2026-02-13 10:46:14 +0100

    feat: border only for character and chicken

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -34,11 +34,15 @@ class MovableObject {
   }
 
   drawFrame(ctx) {
+
+    if(this instanceof Character || this instanceof Chicken)
+    {
     ctx.beginPath();
     ctx.lineWidth = "5";
     ctx.strokeStyle = "blue";
     ctx.rect(this.x, this.y, this.width, this.height);
     ctx.stroke();
+    }
   }
 
   loadImages(arr) {

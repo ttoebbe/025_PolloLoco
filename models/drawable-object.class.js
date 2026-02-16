9ec3c8e6f241ea5ec38f591be7dec27a54267254
Thomas Többe <toebbe.thomas@outlook.de>
2026-02-16 13:34:43 +0100

    feat: drawable Object

---
+++ models/drawable-object.class.js
@@ -0,0 +1,32 @@
+class DrawableObject {
+  x = 120;
+  y = 280;
+  img;
+  height = 150;
+  width = 100;
+  imageCache = {};
+  currentImageIndex = 0;
+
+
+  
+  loadImage(path) {
+    this.img = new Image();
+    this.img.src = path;
+  }
+
+
+  draw(ctx) {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
+
+  
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = img;
+    });
+  }
+
+  
+}
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,12 +1,5 @@
-class MovableObject {
-  x = 120;
-  y = 280;
-  img;
-  height = 150;
-  width = 100;
-  imageCache = {};
+class MovableObject extends DrawableObject {
   speed = 0.15;
-  currentImageIndex = 0;
   otherDirection = false;
   speedY = 0;
   acceleration = 0.5;
@@ -26,14 +19,7 @@ class MovableObject {
     return this.y < 180;
   }
 
-  loadImage(path) {
-    this.img = new Image();
-    this.img.src = path;
-  }
 
-  draw(ctx) {
-    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-  }
 
   drawFrame(ctx) {
     if (this instanceof Character || this instanceof Chicken) {
@@ -74,13 +60,6 @@ class MovableObject {
     return this.energy == 0;
   }
 
-  loadImages(arr) {
-    arr.forEach((path) => {
-      let img = new Image();
-      img.src = path;
-      this.imageCache[path] = img;
-    });
-  }
 
   playAnimation(images) {
     let i = this.currentImageIndex % images.length; // 0, 1, 2, 3, 4, 5 (Modulo-Operator)

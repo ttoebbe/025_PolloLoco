d657f0ab79e047e2229c3a281ac9461bce76cd77
Thomas Többe <toebbe.thomas@outlook.de>
2026-02-16 14:56:19 +0100

    feat: health statusbar aktiv

--- models/world.class.js
+++ models/world.class.js
@@ -26,9 +26,7 @@ class World {
       this.level.enemies.forEach((enemy) => {
         if (this.character.isColliding(enemy)) {
           this.character.hit();
-         
-          console.log("Collision with enemy! Energy: " + this.character.energy);
-          
+          this.statusBar.setPercentage(this.character.energy);  
         } 
       });
     }, 100);
@@ -38,9 +36,15 @@ class World {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
     this.ctx.translate(this.camera_x, 0);
-
     this.addObjectsToMap(this.level.backgroundObjects);
+
+    this.ctx.translate(-this.camera_x, 0); // Zurücksetzen der Kamera-Position für die Statusleiste
+    // ---------------------Space for fixed objects like status bar---------------------
     this.addToMap(this.statusBar);
+
+    //---------------------- End Space for fixed objects like status bar----------------------
+    this.ctx.translate(this.camera_x, 0); // Kamera-Position wiederherstellen für die Spielfiguren
+
     this.addToMap(this.character);
     this.addObjectsToMap(this.level.enemies);
     this.addObjectsToMap(this.level.clouds);

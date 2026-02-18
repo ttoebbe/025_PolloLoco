2fe8792aa4275d2af95860164c0287a274c6cc21
Thomas Toebbe <toebbe.thomas@outlook.de>
2026-02-18 18:45:58 +0100

    fix: Background

--- levels/level1.js
+++ levels/level1.js
@@ -76,7 +76,7 @@ function createLevel1() {
       ),
       new BackgroundObject(
         "img/5_background/layers/1_first_layer/1.png",
-        719 * 2, 
+        719 * 2,
       ),
 
       new BackgroundObject("img/5_background/layers/air.png", 719 * 3),
@@ -91,6 +91,19 @@ function createLevel1() {
       new BackgroundObject(
         "img/5_background/layers/1_first_layer/2.png",
         719 * 3,
+      ),
+      new BackgroundObject("img/5_background/layers/air.png", 719 * 4),
+      new BackgroundObject(
+        "img/5_background/layers/3_third_layer/1.png",
+        719 * 4,
+      ),
+      new BackgroundObject(
+        "img/5_background/layers/2_second_layer/1.png",
+        719 * 4,
+      ),
+      new BackgroundObject(
+        "img/5_background/layers/1_first_layer/1.png",
+        719 * 4,
       ),
     ],
     [

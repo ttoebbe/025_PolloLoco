655d4322405c387282dd97612ac69e017e65e004
Thomas Többe <toebbe.thomas@outlook.de>
2026-02-19 08:08:59 +0100

    enemy to near to each other Fixes #13

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -19,7 +19,7 @@ class Chicken extends MovableObject {
     super().loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
     this.loadImages(this.IMAGES_WALKING);
     this.loadImages(this.IMAGES_DEAD);
-    this.x = 200 + Math.random() * 500;
+    this.x = 300 + Math.random() * 1500;
     this.speed = 0.15 + Math.random() * 0.25;
   }
 

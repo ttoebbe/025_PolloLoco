fa6a1eddee169b9ca488544f4b8ff06f33964756
Thomas Többe <toebbe.thomas@outlook.de>
2026-02-19 09:42:50 +0100

    fix: delete frame and position restart button

--- models/world.class.js
+++ models/world.class.js
@@ -272,7 +272,7 @@ class World {
       this.flipImage(movableObject);
     }
     movableObject.draw(this.ctx);
-    movableObject.drawFrame(this.ctx);
+    // movableObject.drawFrame(this.ctx);
     if (movableObject.otherDirection) {
       this.flipImageBack(movableObject);
     }
